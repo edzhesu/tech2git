f915e145b75f195efd8f5f524f3984572ba50669
Ece <Edzhe.Ayan@student.nhh.no>
2024-09-27 14:38:07 +0200

    Add error handling to argmax

--- dumb.py
+++ dumb.py
@@ -4,5 +4,20 @@ Created on Wed Sep 25 13:05:58 2024
 
 @author: ECE SU
 """
-
-print("dumb")
+def argmax(values):
+    
+ N = len(values)
+ imax= 0
+ Vmax= values[0]
+ for l in range(1,11):
+     v= values[i]
+     if v> vmax:
+        
+        imax= i
+        vmax= v
+        return imax,vmax
+def main():
+    values= [2,3,-1,7,4]
+    imax,vmax= argmax(values)
+    print('flocation of max: (max), value: (vmax)')
+    
